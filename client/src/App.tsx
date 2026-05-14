9fb79c0d3e9055c8fdcb20d8c9eede9447013f18
Ciscoroyal <fboadi600@gmail.com>
2026-05-14 16:38:47 +0000

    Corrected a styling issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <div className='text-xl bg-blue-500-600'>FieldSync Project</div>
+      <div className='text-xl bg-blue-500'>FieldSync Project</div>
     </>
   )
 }
